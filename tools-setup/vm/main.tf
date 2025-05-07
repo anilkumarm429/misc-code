2ad59b408640518e7b996dc51ae2209dc7de5762
Anil <anil@test.com>
2025-05-07 22:12:01 +0530

    roboshop project

--- tools-setup/vm/main.tf
+++ tools-setup/vm/main.tf
@@ -2,7 +2,8 @@ resource "azurerm_public_ip" "publicip" {
   name                = var.name
   location            = var.rg_location
   resource_group_name = var.rg_name
-  allocation_method   = "Static"
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
 
 resource "azurerm_network_interface" "privateip" {
@@ -23,36 +24,64 @@ resource "azurerm_network_interface_security_group_association" "nsg-attach" {
   network_security_group_id = "/subscriptions/7d58fd8a-e8be-4f50-b9f6-03616700d9fc/resourceGroups/project-1/providers/Microsoft.Network/networkSecurityGroups/project-allow-all"
 }
 
-resource "azurerm_virtual_machine" "vm" {
-  name                          = var.name
-  location                      = var.rg_location
-  resource_group_name           = var.rg_name
-  network_interface_ids         = [azurerm_network_interface.privateip.id]
-  vm_size                       = var.vm_size
-  delete_os_disk_on_termination = true
+# ############# We moved to spot instance for saving the cost
+# resource "azurerm_virtual_machine" "vm" {
+#   name                          = var.name
+#   location                      = var.rg_location
+#   resource_group_name           = var.rg_name
+#   network_interface_ids         = [azurerm_network_interface.privateip.id]
+#   vm_size                       = var.vm_size
+#   delete_os_disk_on_termination = true
+#
+#   storage_image_reference {
+#     id = "/subscriptions/7d58fd8a-e8be-4f50-b9f6-03616700d9fc/resourceGroups/project-1/providers/Microsoft.Compute/images/local-devops-practice"
+#   }
+#
+#   storage_os_disk {
+#     name              = "${var.name}-disk"
+#     caching           = "ReadWrite"
+#     create_option     = "FromImage"
+#     managed_disk_type = "Standard_LRS"
+#   }
+#   os_profile {
+#     computer_name  = var.name
+#     admin_username = "azuser"
+#     admin_password = "Devops@12345"
+#   }
+#
+#   os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
+# }
 
-  storage_image_reference {
-    id = "/subscriptions/7d58fd8a-e8be-4f50-b9f6-03616700d9fc/resourceGroups/project-1/providers/Microsoft.Compute/images/local-devops-practice"
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = var.name
+  location                        = var.rg_location
+  resource_group_name             = var.rg_name
+  size                            = var.vm_size
+  admin_username                  = "azuser"
+  admin_password                  = "Devops@12345"
+  disable_password_authentication = false
+  network_interface_ids           = [azurerm_network_interface.privateip.id]
+
+  os_disk {
+    name                 = "${var.name}-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_os_disk {
-    name              = "${var.name}-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = var.name
-    admin_username = "azuser"
-    admin_password = "Devops@12345"
-  }
+  source_image_id = "/subscriptions/7d58fd8a-e8be-4f50-b9f6-03616700d9fc/resourceGroups/project-1/providers/Microsoft.Compute/images/local-devops-practice"
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+
+  # Spot Details
+  priority        = "Spot"
+  max_bid_price   = -1
+  eviction_policy = "Deallocate"
 }
 
+
 resource "azurerm_dns_a_record" "public_dns_record" {
+  depends_on          = [azurerm_linux_virtual_machine.vm]
   name                = var.name
   zone_name           = "apps11.shop"
   resource_group_name = var.rg_name
